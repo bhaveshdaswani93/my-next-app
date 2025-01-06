1693a8677db196f461177aa29dd94412d6f91bd3
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-01-06 21:36:03 +0530

    Update layout and Home component styles; add custom font and background colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-class">
-      <p>Hello world</p>
+    <div className="font-serif">
+      <p className="bg-slate-900 text-white">Hello world</p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
   content: [
@@ -11,7 +12,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        slate: {
+          300: "hsl(212, 45%, 89%)",
+          500: "hsl(216, 15%, 48%)",
+          900: "hsl(218, 44%, 22%)",
+        }
       },
+      fontFamily: {
+        serif: ["Outfit", ...defaultTheme.fontFamily.serif],
+      },
+      
     },
   },
   plugins: [],
