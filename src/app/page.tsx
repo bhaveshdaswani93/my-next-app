c9b15b9ee5223cb61232b653698177ba459d4f13
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-01-10 22:08:40 +0530

    Add greeting message to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-serif">
+      
       <p className="bg-slate-900 text-white">Hello world</p>
     </div>
   );
